c2b22de746aef540aea79f9e085828ff1d376a66
sriramtummala <56696863+sriramtummala@users.noreply.github.com>
2019-12-03 14:43:06 -0600

    Create Node.cs

---
+++ heapImplementation/Node.cs
@@ -0,0 +1,22 @@
+namespace heapImplementation
+{
+    public class Node
+    {
+        private int key;
+
+        public Node(int key)
+        {
+            this.key = key;
+        }
+
+        public int getKey()
+        {
+            return key;
+        }
+
+        public void setKey(int key)
+        {
+            this.key = key;
+        }
+    }
+}
