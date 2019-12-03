c862f6de16805dd67759708e7800194acd3dffa1
sriramtummala <56696863+sriramtummala@users.noreply.github.com>
2019-12-03 14:34:23 -0600

    Create Node.cs

---
+++ singlyLinkedList/Node.cs
@@ -0,0 +1,13 @@
+namespace ds.singlyLinkedList
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public void displayNode()
+        {
+
+        }
+    }
+}
